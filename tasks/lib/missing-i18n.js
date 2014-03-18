8c544d63c57d143b7e23d85cfd54e21992df0849
Robert Barbey <robert.barbey@gmail.com>
2014-03-18 14:54:16 +0100

    Added test for ignored I18n Handlebars helper

--- tasks/lib/missing-i18n.js
+++ tasks/lib/missing-i18n.js
@@ -3,15 +3,38 @@
 var fs = require('fs');
 var sugar = require('sugar');
 
-exports.init = function(grunt) {
-  exports.findMissingI18n = function(files, done) {
+exports.init = function (grunt) {
+  exports.findMissingI18n = function (files, done) {
     var results = {};
 
     for (var i = 0; i < files.length; ++i) {
+      if (fs.lstatSync(files[i]).isDirectory()) {
+        continue;
+      }
+
       var content = fs.readFileSync(files[i], 'utf8');
       var stripped = content.stripTags();
-      if (stripped.lines().length > 0) {
-        results[files[i]] = stripped.lines();
+
+      var lines = stripped.lines();
+      if (lines.length === 0) {
+        continue;
+      }
+
+      var result = [];
+      lines.forEach(function (line) {
+        if (line.isBlank()) {
+          return;
+        }
+
+        if (line.has(/\{\{.*\}\}/)) {
+          return;
+        }
+
+        result.push(line);
+      });
+
+      if (result.length > 0) {
+        results[files[i]] = result;
       }
     }
 
--- test/missing-i18n_test.js
+++ test/missing-i18n_test.js
@@ -17,5 +17,23 @@ exports.missingI18n = {
       test.equal(result[files[0]].length, 1, 'Should report 1 missing translation')
       test.done();
     });
+  },
+  noMissingTranslationInHandlebars: function(test) {
+    test.expect(1);
+
+    var files = [path.join(fixtures, 'i18n.hbs')];
+    missingI18n.findMissingI18n(files, function(result) {
+      test.equal(Object.size(result), 0, 'Should not report missing translation in Handlebars file');
+      test.done();
+    });
+  },
+  ignoresDirectories: function(test) {
+    test.expect(1);
+
+    var files = [fixtures];
+    missingI18n.findMissingI18n(files, function(results) {
+      test.ok(Object.isEmpty(results), 'Should ignore directories');
+      test.done();
+    })
   }
 };
